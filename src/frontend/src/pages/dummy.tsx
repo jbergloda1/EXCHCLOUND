b167e0431d44ba3871b49460067f7c20b46d870e
Truong Pham <pntruong1@tma.com.vn>
2022-08-15 16:24:17 +0700

    example structure

---
+++ src/frontend/src/pages/dummy.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import BaseButton from '../shared/components/buttons/base-button';
+
+export default function DummyPage() {
+  return (
+    <div>
+      <div>
+        <div>--- example coponent button ---</div>
+        <div><BaseButton></BaseButton></div>
+      </div>
+      <div>--------------------------------------</div>
+    </div>
+  );
+}
---
+++ src/frontend/src/shared/components/buttons/base-button.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+export default function BaseButton(){
+  return(
+    <Button variant="contained">Contained</Button>
+  )
+}
